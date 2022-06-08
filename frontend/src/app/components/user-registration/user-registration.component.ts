0e628e413ad54723d86d05080f3deb6e8f5c81fe
nardost <nardos.tessema@gmail.com>
2022-06-08 17:02:47 -0500

    Angular cli upgraded to 14

--- frontend/src/app/components/user-registration/user-registration.component.ts
+++ frontend/src/app/components/user-registration/user-registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'user-registration-form',
@@ -8,9 +8,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class UserRegistrationComponent implements OnInit {
 
-  userRegistrationForm!: FormGroup;
+  userRegistrationForm!: UntypedFormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: UntypedFormBuilder) {
   }
 
   ngOnInit(): void {
