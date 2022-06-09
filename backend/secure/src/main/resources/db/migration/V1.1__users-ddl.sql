0259f0912c1775e8e430c1e1c07fbb0031d31a94
nardost <nardos.tessema@gmail.com>
2022-06-09 15:19:00 -0500

    SECURITY-3 Security configuration class created.

--- backend/secure/src/main/resources/db/migration/V1.1__users-ddl.sql
+++ backend/secure/src/main/resources/db/migration/V1.1__users-ddl.sql
@@ -1,7 +1,7 @@
 create table users(
     id uuid not null unique,
     email varchar(320) not null unique,
-    country_code(6) not null,
+    country_code varchar(6) not null,
     phone_number varchar(13) not null,
     full_name varchar(255) not null,
     password_hash varchar(60),
